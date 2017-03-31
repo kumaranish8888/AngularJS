91271b85854d6fed6bde6713a04e6ca0fda57f6b
kumaranish8888 <kumaranish8888@gmail.com>
2017-03-30 23:47:10 -0400

    Modified file

--- app/app.js
+++ app/app.js
@@ -6,26 +6,22 @@
         
         var registerObj = {
             templateUrl: "app/register/register.tpl.html",
-            controller: "registerCtrl as rc",
-            url : "/register"
+            controller: "registerCtrl as rc"
         };
         
         var productsObj = {
             templateUrl: "app/products/products.tpl.html",
-            controller:"productsCtrl as pc",
-            url : '/products'
+            controller:"productsCtrl as pc"
         };
         
         var postsObj = {
             templateUrl: "app/posts/posts.tpl.html",
-            controller :"postsCtrl as pc",
-            url: '/posts'
+            controller :"postsCtrl as pc"
         };
         
         var loginObj = {
             templateUrl: "app/login/login.tpl.html",
-            controller: "loginCtrl as ll",
-            url: '/login'
+            controller: "loginCtrl as ll"
         };
         
         var homeObj = {
@@ -39,7 +35,7 @@
         $stateProvider.state("login", loginObj);
         $stateProvider.state("home", homeObj);
         
-         $urlRouterProvider.otherwise('/');
+       //  $urlRouterProvider.otherwise('/');
         
     }]);
     
