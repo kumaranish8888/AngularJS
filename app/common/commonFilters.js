d94363a5cb3eae966988f9e583525893f4205773
kumaranish8888 <kumaranish8888@gmail.com>
2017-04-10 15:10:29 -0400

    Modified code

--- app/common/commonFilters.js
+++ app/common/commonFilters.js
@@ -10,7 +10,9 @@
                 return "+1 " +input.substring(0,3)+"-"+input.substring(3,6)+"-"+input.substring(6,10);
             }else if(criteria=="IN"){
                 return "+91 " +input.substring(0,5)+"-"+input.substring(5,10);
-            } else 
+            } else if(criteria == "IR"){
+                return "+21 " +input.substring(0,2)+"-"+input.substring(2,4)+"-"+input.substring(4,6)+"-"+input.substring(6,8)+"-"+input.substring(8,10);
+            }else
                 return input;
         }
     }
